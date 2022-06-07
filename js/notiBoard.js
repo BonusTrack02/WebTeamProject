0a3f433e033628e655e40515c0b202578a6faa92
BonusTrack02 <wlsgud321@gmail.com>
2022-06-07 20:44:31 +0900

    산불 위험 지수 최대, 평균, 최소값 출력

--- js/notiBoard.js
+++ js/notiBoard.js
@@ -4,16 +4,19 @@ $(document).ready(function() {
         if (xhr.readyState==4 && xhr.status==200) {
             var fireData = JSON.parse(xhr.responseText)
             let accordion
+            let accordionIndex = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen"]
             for (let i = 0; i < fireData.response.body.totalCount; i++) {
                 accordion += `<div class="accordion-item">
                 <h2 class="accordion-header" id="headingOne">
                   <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    test
+                    전국 산불 위험 예보 ${fireData.response.body.items.item[i].analdate}
                   </button>
                 </h2>
                 <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                   <div class="accordion-body">
-                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    산불 위험 지수 최댓값 : ${fireData.response.body.items.item[i].maxi} <br>
+                    산불 위험 지수 평균값 : ${fireData.response.body.items.item[i].meanavg} <br>
+                    산불 위험 지수 최솟값 : ${fireData.response.body.items.item[i].mini}
                   </div>
                 </div>
               </div>`
