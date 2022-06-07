b23c4bbcafea0d9acef5870ace59a82e3de23552
BonusTrack02 <wlsgud321@gmail.com>
2022-06-08 07:01:08 +0900

    div id를 배열을 통해 변경

--- js/notiBoard.js
+++ js/notiBoard.js
@@ -7,12 +7,12 @@ $(document).ready(function() {
             let accordionIndex = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen"]
             for (let i = 0; i < fireData.response.body.totalCount; i++) {
                 accordion += `<div class="accordion-item">
-                <h2 class="accordion-header" id="headingOne">
-                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <h2 class="accordion-header" id="heading${accordionIndex[i]}">
+                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${accordionIndex[i]}" aria-expanded="true" aria-controls="collapse${accordionIndex[i]}">
                     전국 산불 위험 예보 ${fireData.response.body.items.item[i].analdate}
                   </button>
                 </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div id="collapse${accordionIndex[i]}" class="accordion-collapse collapse show" aria-labelledby="heading${accordionIndex[i]}" data-bs-parent="#accordionExample">
                   <div class="accordion-body">
                     산불 위험 지수 최댓값 : ${fireData.response.body.items.item[i].maxi} <br>
                     산불 위험 지수 평균값 : ${fireData.response.body.items.item[i].meanavg} <br>
