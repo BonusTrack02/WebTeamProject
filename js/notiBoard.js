f26ee644d16ffa0a3f6eceda7f11078416451e01
BonusTrack02 <wlsgud321@gmail.com>
2022-06-07 20:06:54 +0900

    index.html jquery 추가 공지사항 html 추가 공지사항 js 추가

---
+++ js/notiBoard.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    let xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState==4 && xhr.status==200) {
+            var fireData = JSON.parse(xhr.responseText)
+            let accordion
+            for (let i = 0; i < fireData.response.body.totalCount; i++) {
+                accordion += `<div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    test
+                  </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                  </div>
+                </div>
+              </div>`
+            }
+            document.getElementsByClassName('accordion')[0].innerHTML = accordion
+        }
+    }
+
+    xhr.open("GET", "../fireData.json", true)
+    xhr.send()
+})
