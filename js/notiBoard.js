1ee53ac44aac8e1180fecf9a4cec2d745f318165
BonusTrack02 <wlsgud321@gmail.com>
2022-06-08 13:15:23 +0900

    new json file accordion 수정

--- js/notiBoard.js
+++ js/notiBoard.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
     xhr.onreadystatechange = function() {
         if (xhr.readyState==4 && xhr.status==200) {
             var fireData = JSON.parse(xhr.responseText)
-            let accordion
+            let accordion = ""
             let accordionIndex = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen"]
             for (let i = 0; i < fireData.response.body.totalCount; i++) {
                 accordion += `<div class="accordion-item">
                 <h2 class="accordion-header" id="heading${accordionIndex[i]}">
-                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${accordionIndex[i]}" aria-expanded="true" aria-controls="collapse${accordionIndex[i]}">
-                    전국 산불 위험 예보 ${fireData.response.body.items.item[i].analdate}
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${accordionIndex[i]}" aria-expanded="true" aria-controls="collapse${accordionIndex[i]}">
+                    전국 산불 위험 예보 ${fireData.response.body.items.item[i].analdate}시
                   </button>
                 </h2>
-                <div id="collapse${accordionIndex[i]}" class="accordion-collapse collapse show" aria-labelledby="heading${accordionIndex[i]}" data-bs-parent="#accordionExample">
+                <div id="collapse${accordionIndex[i]}" class="accordion-collapse collapse" aria-labelledby="heading${accordionIndex[i]}" data-bs-parent="#accordionExample">
                   <div class="accordion-body">
                     산불 위험 지수 최댓값 : ${fireData.response.body.items.item[i].maxi} <br>
                     산불 위험 지수 평균값 : ${fireData.response.body.items.item[i].meanavg} <br>
